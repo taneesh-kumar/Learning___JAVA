c61b58c5fdffb4d01aaf147581916129f4441056
taneesh-kumar <heyitstaneesh@gmail.com>
2024-09-27 04:28:05 +0530

    Day-3

--- Loops.java
+++ Loops.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ForLoop {
+public class Loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input;
--- WhileLoop.java
+++ WhileLoop.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class While {
+public class WhileLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
