f57dad88e8af7b8b6d631e7f38d63ba87846b7cf
taneesh-kumar <heyitstaneesh@gmail.com>
2024-09-27 04:20:01 +0530

    Day-3

---
+++ ForLoop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        do{
+            System.out.println("Enter the number");
+            int n = sc.nextInt();
+            for(int i=1; i<=n; i++){
+            System.out.print(i+" ");
+            }
+            System.out.println("Want to continue? yes[1] No[0]");
+            input = sc.nextInt();
+        }while(input == 1);
+    }    
+}
