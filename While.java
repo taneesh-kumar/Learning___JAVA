0c7cbac99b78211709e7f011efd1886394af670d
taneesh-kumar <heyitstaneesh@gmail.com>
2024-09-27 04:26:41 +0530

    Day-3

---
+++ While.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = sc.nextInt();
+        int i=1;
+        while (i<=number) {
+            System.out.print(i+" ");
+            i++;
+        }
+    }    
+}
