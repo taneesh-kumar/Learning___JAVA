c37e54431d223cac0c208ed4f7a005743e014ab2
Taneesh <taneesh@localhost.localdomain>
2025-03-02 00:27:57 +0530

    new

--- Scannerclass.java
+++ Scannerclass.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ScannerClass {
+public class Scannerclass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
