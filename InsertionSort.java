9955729a6acc10db03c2287c6c5b0841977d6dd9
taneesh-kumar <heyitstaneesh@gmail.com>
2025-08-27 09:19:51 +0530

    changes

--- InsertionSort.java
+++ InsertionSort.java
@@ -7,14 +7,14 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = {43, 52, 999, -8, -3, 6, 55, 78, 67};
 
-        for (int i = 0; i < arr.length; i++) {
-            int current = arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            int greater = arr[i];
             int j = i-1;
-            while (j>=0 && current < arr[j]) {
+            while (j>=0 && greater < arr[j]) {
                 arr[j+1] = arr[j];
                 j--;
             }
-            arr[j+1] = current;
+            arr[j+1] = greater;
         }
         PrintArrray(arr);
     }
